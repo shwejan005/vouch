038d670f78fce9d34ec4ed4fa06c8f632221f0ba
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-01-26 22:15:57 +0530

    Shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
 export default function Home() {
   return (
     <div>
-      Home Page
+      <h1>Home Page</h1>
+      <Button>Sign In</Button>
     </div>
   );
 }
