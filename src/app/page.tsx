cede9574bf1d7711b949006725d717c7610d3a44
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-01-27 20:37:22 +0530

    Added Mode Toggle and few changes to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
-
 export default function Home() {
   return (
     <div>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton>
-          <Button>
-            Sign In
-          </Button>
-        </SignInButton>
-      </SignedOut>
+      Home Page
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <div className='border-b'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar;
