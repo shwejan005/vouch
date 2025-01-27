bdc52cf49f49c04367fa082ad1c1dbd9ed582b81
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-01-27 20:37:52 +0530

    Working on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,11 @@ import Link from 'next/link';
 function Navbar() {
   return (
     <div className='border-b'>
-        Navbar
+        <div className='flex min-w-screen h-15 items-center px-4 container mx-auto'>
+            <Link href='/'>
+                <h1 className='text-red-800'>Vouch</h1>
+            </Link>
+        </div>
     </div>
   )
 }
