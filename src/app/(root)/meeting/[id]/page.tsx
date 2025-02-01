a6ad0671db706acf18174e6a93057f7336b48cf9
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-02-01 21:59:32 +0530

    Working on Meetings page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+'use client'
+import LoaderUI from '@/components/ui/LoaderUI'
+import { useUser } from '@clerk/nextjs'
+import { useParams } from 'next/navigation'
+import React, { useState } from 'react'
 
 function Meetings() {
+  const { id } = useParams()
+  const { isLoaded } = useUser()
+  const isCallLoading = false
+  const [isSetupComplete, setisSetupComplete] = useState(false)
+
+  if (!isLoaded || isCallLoading ) return <LoaderUI />
+
   return (
     <div>Meetings</div>
   )
--- src/components/ui/LoaderUI.tsx
+++ src/components/ui/LoaderUI.tsx
@@ -4,7 +4,7 @@ function LoaderUI() {
   // h-16 + 1 for border in navbar => 65px
   return (
     <div className="h-[calc(100vh-4rem-1px)] flex items-center justify-center">
-      <LoaderIcon className="h-8 w-8 animate-spin text-muted-foreground" />
+      <LoaderIcon className="text-red-600 h-8 w-8 animate-spin text-muted-foreground" />
     </div>
   );
 }
