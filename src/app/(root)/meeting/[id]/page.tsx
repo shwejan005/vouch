800223f3d1ad2b7505eb958f9a9e4b4b6ccb4918
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-02-01 22:04:24 +0530

    get call by id hook implemented

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,19 +1,29 @@
 'use client'
+import MeetingRoom from '@/components/MeetingRoom'
+import MeetingSetup from '@/components/MeetingSetup'
 import LoaderUI from '@/components/ui/LoaderUI'
+import useGetCallById from '@/hooks/useGetCallById'
 import { useUser } from '@clerk/nextjs'
+import { StreamCall } from '@stream-io/video-react-sdk'
 import { useParams } from 'next/navigation'
 import React, { useState } from 'react'
 
 function Meetings() {
   const { id } = useParams()
   const { isLoaded } = useUser()
-  const isCallLoading = false
-  const [isSetupComplete, setisSetupComplete] = useState(false)
-
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+  const { call, isCallLoading } = useGetCallById()
   if (!isLoaded || isCallLoading ) return <LoaderUI />
 
   return (
-    <div>Meetings</div>
+    <StreamCall call = {call}>
+      { !isSetupComplete ? (
+        <MeetingSetup onSetupComplete={() => setIsSetupComplete(true)} />
+      )
+      : (
+        <MeetingRoom />
+      )}
+    </StreamCall>
   )
 }
 
