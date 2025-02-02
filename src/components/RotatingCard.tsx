736b37b2b01cd1161dcc2dd9786266b547566c69
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-02-03 00:05:48 +0530

    Added few functions

--- src/components/RotatingCard.tsx
+++ src/components/RotatingCard.tsx
@@ -1,9 +1,12 @@
+import { calculateRecordingDuration } from '@/lib/utils'
 import { CallRecording } from '@stream-io/video-react-sdk'
+import { format } from 'date-fns'
 import React from 'react'
 import toast from 'react-hot-toast'
 
 function RotatingCard( {recording}: { recording: CallRecording }) {
-  const handleText = async() => {
+
+  const handleCopyLink = async() => {
     try {
       await navigator.clipboard.writeText(recording.url)
       toast.success('URL copied to clipboard')
@@ -11,6 +14,16 @@ function RotatingCard( {recording}: { recording: CallRecording }) {
       toast.error('Failed to copy URL')
     }
   }
+
+  const formattedStartTime = recording.start_time
+    ? format(new Date(recording.start_time), "MMM d, yyyy, hh:mm a")
+    : "Unknown";
+
+  const duration =
+    recording.start_time && recording.end_time
+      ? calculateRecordingDuration(recording.start_time, recording.end_time)
+      : "Unknown duration";
+
   return (
     <div>RotatingCard</div>
   )
