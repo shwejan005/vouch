d3763943e90a06538654e1c27ca0990e58622b2e
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-02-03 22:34:07 +0530

    Working on schedules page

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,8 +1,20 @@
+import InterviewScheduleUI from '@/components/InterviewScheduleUI'
+import LoaderUI from '@/components/ui/LoaderUI'
+import { useUserRole } from '@/hooks/useUserRole'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function Schedule() {
+  const router = useRouter()
+  const { isInterviewer,isLoading } = useUserRole()
+
+  if (isLoading) return <LoaderUI />
+  if (!isInterviewer) router.push('/')
+
   return (
-    <div>Schedule</div>
+    <div>
+      <InterviewScheduleUI />
+    </div>
   )
 }
 
---
+++ src/components/InterviewScheduleUI.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InterviewScheduleUI() {
+  return (
+    <div>InterviewScheduleUI</div>
+  )
+}
+
+export default InterviewScheduleUI
